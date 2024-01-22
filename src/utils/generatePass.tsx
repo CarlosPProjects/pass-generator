23be833a8a4cc14a56e89bd03a9f3d015bb38057
CarlosPProjects <cg9822@gmail.com>
2024-01-22 14:31:12 +0100

    generatePassword function created

---
+++ src/utils/generatePass.tsx
@@ -0,0 +1,9 @@
+const generatePassword = () => {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    for (let i = 0; i < 12; i++) {
+        password += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    return password;
+}
