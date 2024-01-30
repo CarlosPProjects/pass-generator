951b43c061e9626220c12274b6cd5d45e4e27e83
CarlosPProjects <cg9822@gmail.com>
2024-01-30 11:51:36 +0100

    refactor function - generatePassword

--- src/utils/generatePass.tsx
+++ src/utils/generatePass.tsx
@@ -1,5 +1,5 @@
 export const generatePassword = () => {
-    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@?¿";
     let password = "";
     for (let i = 0; i < 12; i++) {
         password += chars[Math.floor(Math.random() * chars.length)];
