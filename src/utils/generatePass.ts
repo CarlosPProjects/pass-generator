f8b717b1caab2d924de3b1e186d5323fac340496
CarlosPProjects <cg9822@gmail.com>
2024-02-08 09:48:55 +0100

    added getOptionsLenght functionality to know if all options are enabled

--- src/utils/generatePass.ts
+++ src/utils/generatePass.ts
@@ -21,6 +21,18 @@ export default class Password {
         this.length = options.length || 12;
     }
 
+    getOptionLength(): number {
+
+        let counter = 0;
+
+        this.lowercase && counter++
+        this.uppercase && counter++
+        this.numbers && counter++
+        this.symbols && counter++
+
+        return counter;
+    }
+
     generate(): string {
         let password = "";
         const characters = this.lowercase + this.uppercase + this.numbers + this.symbols;
